58dc222d44b8015d825170771790c184aaec1df2
YB970902 <78996787+YB970902@users.noreply.github.com>
2023-06-17 02:13:17 +0900

    기능 추가 - 길찾기 테스트용 코드 추가

---
+++ Assets/Scripts/Tester/PathFindTest.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFindTest : MonoBehaviour
+{
+    [SerializeField] TileTest prefabTile = null;
+
+    [SerializeField] int obstacleCount = 0;
+
+    private List<TileTest> tileTestList = new List<TileTest>(TileManager.WidthCount * TileManager.HeightCount);
+    
+    void Start()
+    {
+        for(int y = 0; y < TileManager.HeightCount; ++y)
+        {
+            for(int x = 0; x < TileManager.WidthCount; ++x)
+            {
+                var tile = Instantiate(prefabTile);
+                tile.SetColor(TileTest.TileTestColorTag.Default);
+                tile.SetTilePosition(x + y * TileManager.WidthCount);
+                tileTestList.Add(tile);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int startIndex = 0;
+            int destIndex = TileManager.WidthCount * TileManager.HeightCount - 1;
+
+            for (int y = 0; y < TileManager.HeightCount; ++y)
+            {
+                for (int x = 0; x < TileManager.WidthCount; ++x)
+                {
+                    var index = x + y * TileManager.WidthCount;
+
+                    TileManager.Instance.PathFinder.SetObstacle(index, false);
+                    tileTestList[index].SetColor(TileTest.TileTestColorTag.Default);
+                }
+            }
+
+            for(int i = 0; i < obstacleCount; ++i)
+            {
+                int randomIndex = Random.Range(0, TileManager.WidthCount * TileManager.HeightCount);
+
+                if (randomIndex == startIndex || randomIndex == destIndex) continue;
+
+                TileManager.Instance.PathFinder.SetObstacle(randomIndex, true);
+                tileTestList[randomIndex].SetColor(TileTest.TileTestColorTag.Obstacle);
+            }
+
+            var path = TileManager.Instance.PathFinder.FindPath(startIndex, destIndex);
+
+            path?.ForEach(index => tileTestList[index].SetColor(TileTest.TileTestColorTag.Path));
+            tileTestList[startIndex].SetColor(TileTest.TileTestColorTag.Start);
+            tileTestList[destIndex].SetColor(TileTest.TileTestColorTag.Dest);
+        }
+    }
+}
---
+++ Assets/Scripts/Tester/TileTest.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTest : MonoBehaviour
+{
+    [SerializeField] Color clrDefault = Color.white;
+    [SerializeField] Color clrPath = Color.gray;
+    [SerializeField] Color clrStart = Color.red;
+    [SerializeField] Color clrDest = Color.blue;
+    [SerializeField] Color clrObstacle = Color.black;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+
+    [SerializeField] float tileSize = 1.0f;
+
+    public enum TileTestColorTag
+    {
+        Default,
+        Path,
+        Start,
+        Dest,
+        Obstacle,
+    }
+
+    public int Index { get; private set; }
+
+    private void Awake()
+    {
+        spriteRenderer.color = clrDefault;
+    }
+
+    public void SetTilePosition(int index)
+    {
+        Index = index;
+
+        Vector2 pos = Vector2.zero;
+
+        pos.x = index % TileManager.WidthCount * tileSize;
+        pos.y = index / TileManager.WidthCount * tileSize;
+
+        transform.position = pos;
+    }
+
+    public void SetColor(TileTestColorTag tag)
+    {
+        Color color;
+
+        switch(tag)
+        {
+            case TileTestColorTag.Default:
+                color = clrDefault;
+                break;
+            case TileTestColorTag.Path:
+                color = clrPath;
+                break;
+            case TileTestColorTag.Start:
+                color = clrStart;
+                break;
+            case TileTestColorTag.Dest:
+                color = clrDest;
+                break;
+            case TileTestColorTag.Obstacle:
+                color = clrObstacle;
+                break;
+            default:
+                color = clrDefault;
+                break;
+        }
+
+        spriteRenderer.color = color;
+    }
+}
