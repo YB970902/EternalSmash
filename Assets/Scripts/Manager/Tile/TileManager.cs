7f845cf4e0bfe14f06bf4ff7bde0e0075bf86a4e
YB970902 <78996787+YB970902@users.noreply.github.com>
2023-07-08 15:01:39 +0900

    기능 추가 - 타일매니저에서 인덱스를 받아 위치를 반환하는 기능 추가

--- Assets/Scripts/Manager/Tile/TileManager.cs
+++ Assets/Scripts/Manager/Tile/TileManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using FixMath.NET;
 using UnityEngine;
 
 public interface IPathFinder
@@ -22,18 +23,47 @@ public interface IPathFinder
 
 public class TileManager : MonoSingleton<TileManager>
 {
-    public static int WidthCount = 100;
-    public static int HeightCount = 100;
+    public const int WidthCount = 100;
+    public const int HeightCount = 100;
+    public const int TotalCount = WidthCount * HeightCount;
 
-    public static int TotalCount = WidthCount * HeightCount;
-
+    private static readonly FixVector2 TileSize = new FixVector2(1, 1);
+    private static readonly FixVector2 HalfTileSize = new FixVector2(0.5f, 0.5f);
+    
     private IPathFinder pathFinder;
 
     public IPathFinder PathFinder => pathFinder;
 
     public override void Init()
     {
-        pathFinder = new FinderJPS();
+        pathFinder = new FinderAstar();
         pathFinder.Init();
+    }
+
+    /// <summary>
+    /// 타일의 위치 반환.
+    /// </summary>
+    public FixVector2 GetTilePosition(int _index)
+    {
+        if (_index < 0 || _index >= TotalCount)
+        {
+            Debug.LogError("Out of range");
+            return FixVector2.Zero;
+        }
+        
+        (int x, int y) = IndexToPosition(_index);
+        
+        return new FixVector2(TileSize.x * (Fix64)x + HalfTileSize.x, TileSize.y * (Fix64)y + HalfTileSize.y);
+    }
+
+    private (int, int) IndexToPosition(int _index)
+    {
+        if (_index < 0 || _index >= TotalCount)
+        {
+            Debug.LogError("Out of range");
+            return (0, 0);
+        }
+
+        return (_index % WidthCount, _index / WidthCount);
     }
 }
