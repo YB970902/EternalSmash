bd84de7d3b18112de0e0dd3d8018cc41c4d8b40b
YB970902 <78996787+YB970902@users.noreply.github.com>
2023-07-08 15:02:14 +0900

    기능 추가 - 전투 관리를 위한 배틀 매니저 추가

---
+++ Assets/Scripts/Manager/Tile/BattleManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+
+namespace Battle
+{
+    public class BattleManager : MonoSingleton<BattleManager>
+    {
+        [SerializeField] private List<MovementController> movementControllers;
+        public override void Init()
+        {
+            for (int i = 0, count = movementControllers.Count; i < count; ++i)
+            {
+                movementControllers[i].Init();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            for (int i = 0, count = movementControllers.Count; i < count; ++i)
+            {
+                movementControllers[i].Tick();
+            }
+        }
+    }
+}
