44b849e02db903099f6c14214e81c6c1e373ef70
YB970902 <78996787+YB970902@users.noreply.github.com>
2023-07-08 18:46:36 +0900

    기능 추가 - PathGuide에 알맞은 테스트를 위한 기능 추가

--- Assets/Scripts/Tester/CharacterMoveTest.cs
+++ Assets/Scripts/Tester/CharacterMoveTest.cs
@@ -5,26 +5,46 @@ using Battle;
 using Character;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CharacterMoveTest : MonoBehaviour
 {
+    [SerializeField] private int obstacleCount = 0;
     [SerializeField] private int nextNodeIndex = 0;
-    [SerializeField] private MovementController movement;
+    [SerializeField] private MovementController movement = null;
+    [SerializeField] private PathGuide guide = null;
 
     private void Start()
     {
         var mgr = BattleManager.Instance;
+        guide.Init();
+        guide.Set(movement);
+        guide.SetStartIndex(0);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            movement.SetPosition(nextNodeIndex);
+            guide.SetStartIndex(nextNodeIndex);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            movement.SetNextTile(nextNodeIndex);
+            guide.SetTargetIndex(nextNodeIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var pathFinder = TileManager.Instance.PathFinder;
+            for (int i = 0; i < TileManager.TotalCount; ++i)
+            {
+                pathFinder.SetObstacle(i, false);
+            }
+
+            for (int i = 0; i < obstacleCount; ++i)
+            {
+                pathFinder.SetObstacle(Random.Range(1, TileManager.TotalCount - 1), true);
+            }
         }
     }
 }
